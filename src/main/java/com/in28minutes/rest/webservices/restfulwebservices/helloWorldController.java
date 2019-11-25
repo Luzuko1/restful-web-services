8ec0b6ecc876f5cb59f14b067f102fc52524a9e1
Luzuko1 <Luzukondl@gmail.com>
2019-11-25 15:33:33 +0200

    Initial commit

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloWorldController.java
@@ -0,0 +1,28 @@
+package com.in28minutes.rest.webservices.restfulwebservices;
+
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+public class helloWorldController {
+    //GET
+    //URL - /hello-world
+    //methood - "Hello World
+    @GetMapping( path = "/hello-world")
+    public String helloWorld()
+    {
+        return "Hello World";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean()
+    {
+        return new HelloWorldBean("Hello World ");
+    }
+    // /hello-world-bean/path-variable/luzuko
+    @GetMapping(path = "/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name)
+    {
+        return new HelloWorldBean(String.format("Hello World, %s. You good?", name));
+    }
+}
